7301f3480fc1c4690161eb87466dbeb70e30892e
Salvatore <zoomsulweb@gmail.com>
2021-10-02 13:04:36 +1000

    02/10

--- backend/config.ts
+++ backend/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    PORT: process.env.PORT || 4242,
+    PORT: process.env.PORT || 5000,
     MONGODB_URL: process.env.MONGODB_URL,
     YourSecretEmail: process.env.YourSecretEmail,
     PasswordToSendEmail: process.env.PasswordToSendEmail,
